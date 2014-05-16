7cc2f2a1d7674c7c0e6a06fde754ce1e281d7a63
Nicolas Brucchieri <niguibru@MacBook-Pro-de-Nicolas.local>
2014-05-16 15:03:27 -0300

    “my2”

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/matchesComplete', matchesServicesRoute.matchesComplete);
 app.get('/matches', matchesServicesRoute.matches);
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('Server started on port 3000')
   console.log('http://localhost:3000/')
 });
