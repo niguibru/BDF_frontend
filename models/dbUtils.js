9876b925579043ab231bf9dc24ee60c7d37d918b
Nicolas Brucchieri <niguibru@MacBook-Pro-de-Nicolas.local>
2014-05-16 15:07:04 -0300

    “my3”

--- models/dbUtils.js
+++ models/dbUtils.js
@@ -1,7 +1,7 @@
 var mongoClient = require('mongodb').MongoClient;
 
-var dbUrl = 'mongodb://bdfadmin:bdfargenta@oceanic.mongohq.com:10004/bochadefutbol';
-//var dbUrl = process.env.DB_URL;
+//var dbUrl = 'mongodb://bdfadmin:bdfargenta@oceanic.mongohq.com:10004/bochadefutbol';
+var dbUrl = process.env.DB_URL;
 
 exports.connectToDb = function(fnc) {
   mongoClient.connect(dbUrl, fnc)
