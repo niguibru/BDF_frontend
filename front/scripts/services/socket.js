7d15f1ae86fb3e6a49316eddc708dc1a4ed07400
Nicolas Brucchieri <niguibru@MacBook-Pro-de-Nicolas.local>
2014-05-27 01:51:42 -0300

    “my9”

--- front/scripts/services/socket.js
+++ front/scripts/services/socket.js
@@ -2,7 +2,7 @@
 
 angular.module('bochaDeFutbolApp')
   .factory('socket', function () {
-    var socket = io.connect('http://localhost:3000');
-    //var socket = io.connect('http://www.bochadefutbol.com.ar/');
+    //var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('http://www.bochadefutbol.com.ar/');
     return socket;
   });
