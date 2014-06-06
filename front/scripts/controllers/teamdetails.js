505ad0b96a7137955f46918de61894e35114f0a8
Nicolas Brucchieri <niguibru@MacBook-Pro-de-Nicolas.local>
2014-06-06 20:23:11 -0300

    “my11”

--- front/scripts/controllers/teamdetails.js
+++ front/scripts/controllers/teamdetails.js
@@ -1,11 +1,27 @@
 'use strict';
 
 angular.module('bochaDeFutbolApp')
-  .controller('TeamdetailsCtrl', function ($scope, $routeParams, teams, tweets) {
+  .controller('TeamdetailsCtrl', function ($scope, $rootScope, $routeParams, teams, tweets) {
     
-    teams.getByNameId($routeParams.nameid, function(teamData){
-      $scope.team = teamData;
-    });
+    if ($rootScope.teamsData.length == 0) 
+      getAllTeams();
+    else
+      getTeamDetails();
+    
+    function getTeamDetails() {
+      $rootScope.teamsData.forEach(function(teamData) {
+        if (teamData.nameId == $routeParams.nameid) {
+          $scope.team = teamData;
+        }
+      });
+    }
+    
+    function getAllTeams () {
+      teams.get(function(data){
+        $rootScope.teamsData = data;
+        getTeamDetails();
+      })
+    }
     
     $scope.teamTwts = [];
     tweets.getTweets('"bdf_'+$routeParams.nameid + '"', 5, function(twtsData){
