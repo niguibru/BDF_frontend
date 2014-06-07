ecbb3d53a2d94ad42d43e81bc73b09c56ecaec3a
Nicolas Brucchieri <niguibru@MacBook-Pro-de-Nicolas.local>
2014-06-07 14:50:27 -0300

    “my9”

--- controllers/socketService.js
+++ controllers/socketService.js
@@ -12,7 +12,8 @@ exports.start = function(server) {
 
     // @bochadefutbol TWITS >>
     // get the last 5 tweets when bochadefutbol is mentioned
-    twitter.getTweets('bochadefutbol', 5, function(reply) {
+    twitter.getTweets('bochadefutbol', 10, function(reply) {
+      console.log(reply);
       for (var i = 0; i < reply.statuses.length; i++) {
         var status = reply.statuses[i];
         socket.emit('prevTwits', { 
@@ -26,6 +27,7 @@ exports.start = function(server) {
     })
     // Stream when bochadefutbol is mentioned
     twitter.streamTweets('bochadefutbol', function(tweet) {
+      console.log(tweet);
       if ((tweet.user.screen_name != 'bochadefutbol') && (tweet.text.indexOf('@bochadefutbol') > -1)) {
         socket.emit('newTwits', { 
           tweet_id: tweet.id_str,
@@ -38,6 +40,7 @@ exports.start = function(server) {
     })
     // Stream when @bochadefutbol twit
     twitter.streamUsers('bochadefutbol', function(tweet) {
+      console.log(tweet);
       socket.emit('newTwits',  { 
         tweet_id: tweet.id_str,
         created_at: tweet.created_at,
