708973b1454e6c5ec77d65bd575ed1e71efe973f
Nicolas Brucchieri <niguibru@MacBook-Pro-de-Nicolas.local>
2014-06-14 19:09:40 -0300

    “my12”

--- front/scripts/controllers/main.js
+++ front/scripts/controllers/main.js
@@ -1,12 +1,31 @@
 'use strict';
 
 angular.module('bochaDeFutbolApp')
-  .controller('MainCtrl', function ($scope, $rootScope) {
+  .controller('MainCtrl', function ($scope, $rootScope, $location) {
 
 //    $scope.insta = [];
 //    instagram.getExample('fifa2014', function(instaData){
 //      $scope.insta = instaData;
 //    });
 //    
-        
+    
+    // Socket
+    $rootScope.socketConnect();
+    $rootScope.socket.on('actualizeMatch', function (data) {
+      for (var i = 0; i < $rootScope.allMatches.length; i++) {
+        if ($rootScope.allMatches[i].numId == data.match.numId) {
+          $rootScope.allMatches[i] = data.match;
+          break;
+        }
+      }
+      $scope.$apply();
+    });
+    
+    $scope.go = function ( path ) {
+      $location.path( path );
+    };
+    
+    $scope.yesterdayFilter = '2014/06/13';
+    $scope.todayFilter = '2014/06/14';
+    $scope.tomorrowFilter = '2014/06/15';
   });
