e2f6cc670b86da6dbb8944175ff77d1ec31b306a
Nicolas Brucchieri <niguibru@MacBook-Pro-de-Nicolas.local>
2014-06-14 02:59:11 -0300

    “my12”

--- front/scripts/controllers/matchdetails.js
+++ front/scripts/controllers/matchdetails.js
@@ -37,6 +37,7 @@ angular.module('bochaDeFutbolApp')
       }
       initVars();
       getMatchDetails();
+      setLiveMinute();
       $scope.$apply();
     });
     
@@ -84,6 +85,7 @@ angular.module('bochaDeFutbolApp')
     function setLiveMinute(){
       if ($scope.match.live_minute == '') {
         $scope.live_minuteShow = 'hidden';
+      } else {
         $scope.live_minute = $scope.match.live_minute;
         $scope.live_minute_percent = $scope.live_minute * 100 / 90;
       }
@@ -91,79 +93,83 @@ angular.module('bochaDeFutbolApp')
     
     // set Goal Events_and save to Event Array
     function setGoalEvents_toEventArray(matchData){
-      matchData.events.goals.forEach(function(goal) {
-        var local = '';
-        var visitor = '';
-        var isOwnGoal = false;
-        var isPenaltyGoal = false;
-        var local_showGoal = false;
-        var visitor_showGoal = false;
-        var local_showOwnGoal = false;
-        var local_showPenalty = false;
-        var visitor_showOwnGoal = false;
-        var visitor_showPenalty = false;
-        
-        // If its own goal
-        if (goal.action_type == '6') isOwnGoal = true;
-        // If its penalty goal
-        if (goal.action_type == '2') isPenaltyGoal = true;
-        
-        if ((goal.team == 'local' && !isOwnGoal) || (isOwnGoal && goal.team == 'visitor')) {
-          local = goal.player;
-          local_showGoal = true
-          if (isPenaltyGoal) local_showPenalty = true;
-          if (isOwnGoal) local_showOwnGoal = true;
-        }
-        if ((goal.team == 'visitor' && !isOwnGoal) || (isOwnGoal && goal.team == 'local')) {
-          visitor = goal.player;
-          visitor_showGoal = true
-          if (isPenaltyGoal) visitor_showPenalty = true;
-          if (isOwnGoal) visitor_showOwnGoal = true;
-        }
-        $scope.events.push({
-          local_showGoal: local_showGoal,
-          visitor_showGoal: visitor_showGoal,
-          local_showOwnGoal: local_showOwnGoal,
-          visitor_showOwnGoal: visitor_showOwnGoal,
-          local_showPenalty: local_showPenalty,
-          visitor_showPenalty: visitor_showPenalty,
-          local: local,
-          min: goal.minute,
-          visitor: visitor
+      if (matchData.events.goals != undefined){
+        matchData.events.goals.forEach(function(goal) {
+          var local = '';
+          var visitor = '';
+          var isOwnGoal = false;
+          var isPenaltyGoal = false;
+          var local_showGoal = false;
+          var visitor_showGoal = false;
+          var local_showOwnGoal = false;
+          var local_showPenalty = false;
+          var visitor_showOwnGoal = false;
+          var visitor_showPenalty = false;
+
+          // If its own goal
+          if (goal.action_type == '6') isOwnGoal = true;
+          // If its penalty goal
+          if (goal.action_type == '2') isPenaltyGoal = true;
+
+          if ((goal.team == 'local' && !isOwnGoal) || (isOwnGoal && goal.team == 'visitor')) {
+            local = goal.player;
+            local_showGoal = true
+            if (isPenaltyGoal) local_showPenalty = true;
+            if (isOwnGoal) local_showOwnGoal = true;
+          }
+          if ((goal.team == 'visitor' && !isOwnGoal) || (isOwnGoal && goal.team == 'local')) {
+            visitor = goal.player;
+            visitor_showGoal = true
+            if (isPenaltyGoal) visitor_showPenalty = true;
+            if (isOwnGoal) visitor_showOwnGoal = true;
+          }
+          $scope.events.push({
+            local_showGoal: local_showGoal,
+            visitor_showGoal: visitor_showGoal,
+            local_showOwnGoal: local_showOwnGoal,
+            visitor_showOwnGoal: visitor_showOwnGoal,
+            local_showPenalty: local_showPenalty,
+            visitor_showPenalty: visitor_showPenalty,
+            local: local,
+            min: goal.minute,
+            visitor: visitor
+          });
         });
-      });
+      }
     }
     
     // set Cards Events_and save to Event Array
     function setCardsEvents_toEventArray(matchData){
-      matchData.events.cards.forEach(function(cards) {
-        var local = '';
-        var visitor = '';
-        var local_showYCard = false;
-        var visitor_showYCard = false;
-        var local_showRCard = false;
-        var visitor_showRCard = false;
-        // segunda amarilla y roja es cards.action_type == '4'
-        if (cards.team == 'local') {
-          local = cards.player;
-          if (cards.action_type == '5') local_showYCard = true
-          else local_showRCard = true;
-        }
-        if (cards.team == 'visitor') {
-          visitor = cards.player;
-          if (cards.action_type == '5') visitor_showYCard = true
-          else visitor_showRCard = true;
-        }
-        $scope.events.push({
-          local_showYCard: local_showYCard,
-          visitor_showYCard: visitor_showYCard,
-          local_showRCard: local_showRCard,
-          visitor_showRCard: visitor_showRCard,
-          local: local,
-          min: cards.minute,
-          visitor: visitor
+      if (matchData.events.cards != undefined) {
+        matchData.events.cards.forEach(function(cards) {
+          var local = '';
+          var visitor = '';
+          var local_showYCard = false;
+          var visitor_showYCard = false;
+          var local_showRCard = false;
+          var visitor_showRCard = false;
+          // segunda amarilla y roja es cards.action_type == '4'
+          if (cards.team == 'local') {
+            local = cards.player;
+            if (cards.action_type == '5') local_showYCard = true
+            else local_showRCard = true;
+          }
+          if (cards.team == 'visitor') {
+            visitor = cards.player;
+            if (cards.action_type == '5') visitor_showYCard = true
+            else visitor_showRCard = true;
+          }
+          $scope.events.push({
+            local_showYCard: local_showYCard,
+            visitor_showYCard: visitor_showYCard,
+            local_showRCard: local_showRCard,
+            visitor_showRCard: visitor_showRCard,
+            local: local,
+            min: cards.minute,
+            visitor: visitor
+          });
         });
-      });
+      }
     }
     
     // set Changes Events_and save to Event Array
